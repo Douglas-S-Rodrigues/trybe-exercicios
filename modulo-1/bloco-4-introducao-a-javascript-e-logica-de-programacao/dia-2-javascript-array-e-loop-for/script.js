1c675d5454784fc556d24d5a4279e3f77cebf61d
Douglas Rodrigues <douglas.rodrigues.fw@gmail.com>
2021-11-29 18:37:13 -0300

    adiciona condicao if/else

--- modulo-1/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ modulo-1/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -2,10 +2,16 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let somaDosValores = 0;
 let resultadoFinal = 0;
 let media = 0;
-for(let i = 0; i < numbers.length; i += 1){
+for (let i = 0; i < numbers.length; i += 1) {
   somaDosValores = somaDosValores + numbers[i];
 }
-  resultadoFinal = somaDosValores;
-  media = resultadoFinal / numbers.length;
-  console.log("A media da soma dos valores do array é:" + media);
-  
+resultadoFinal = somaDosValores;
+media = resultadoFinal / numbers.length;
+
+if (media > 20) {
+  console.log("valor maior que 20");
+} else {
+  console.log("valor menor ou igual a 20");
+}
+
+console.log(`A media da soma dos valores do array é: ${media}`);
