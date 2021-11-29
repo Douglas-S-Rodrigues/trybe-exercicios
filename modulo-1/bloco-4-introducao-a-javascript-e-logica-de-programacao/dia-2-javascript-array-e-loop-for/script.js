2a566089460b82c7f49fe9808e2b8705db790b6f
Douglas Rodrigues <douglas.rodrigues.fw@gmail.com>
2021-11-29 18:24:18 -0300

    adicona media do resultado total do array

--- modulo-1/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ modulo-1/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -1,8 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let somaDosValores = 0;
 let resultadoFinal = 0;
+let media = 0;
 for(let i = 0; i < numbers.length; i += 1){
   somaDosValores = somaDosValores + numbers[i];
 }
   resultadoFinal = somaDosValores;
-  console.log(resultadoFinal);
+  media = resultadoFinal / numbers.length;
+  console.log("A media da soma dos valores do array é:" + media);
+  
