f404e90a70382f868c10810e7f9ce0bfc4dcccdf
Douglas Rodrigues <douglas.rodrigues.fw@gmail.com>
2021-11-29 18:19:13 -0300

    soma todos valores contidos no array e mostra o resultado final

--- modulo-1/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ modulo-1/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -1,5 +1,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-
+let somaDosValores = 0;
+let resultadoFinal = 0;
 for(let i = 0; i < numbers.length; i += 1){
-  console.log(numbers[i])
+  somaDosValores = somaDosValores + numbers[i];
 }
+  resultadoFinal = somaDosValores;
+  console.log(resultadoFinal);
