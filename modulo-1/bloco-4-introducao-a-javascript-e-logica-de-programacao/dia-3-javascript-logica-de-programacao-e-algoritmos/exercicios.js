3947c44da1433757de99b1f3cc8123436f9708f2
Douglas Rodrigues <douglas.rodrigues.fw@gmail.com>
2021-11-30 18:53:36 -0300

    adiciona exercicio 1

--- modulo-1/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios.js
+++ modulo-1/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios.js
@@ -1,7 +1,13 @@
 let n = 5;
-let aste = '';
+let aste = '*';
+let linha = '';
 
-for (let i = 0; i <= n; i += 1) {
-  aste += '*';
-  console.log(aste);
+
+for (let index = 0; index < n; index += 1) {
+  linha = linha + aste;
 }
+for(let index = 0; index < n; index += 1) {
+  console.log(linha);
+}
+
+
