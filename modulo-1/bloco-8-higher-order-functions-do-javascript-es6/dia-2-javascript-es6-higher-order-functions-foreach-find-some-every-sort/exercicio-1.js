7f4147ed05ad127bdbd383ad92149c36a4bdf860
Douglas Rodrigues <douglas.rodrigues.fw@gmail.com>
2022-01-20 18:56:05 -0300

    parte  1 resolvido

--- modulo-1/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicio-1.js
+++ modulo-1/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicio-1.js
@@ -61,4 +61,9 @@ const books = [
   },
 ];
 
-// Adicione o código do exercício aqui:
+// 1 - Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947.
+
+
+function authorBornIn1947() {
+  return books.find((book) => book.author.birthYear === 1947).author.name;
+}
